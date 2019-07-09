f05bdcdf2fa68db65adfbe35638b235e7d567795
Vincent Belfoure <vbelfoure@hotmail.com>
2019-07-08 20:49:23 -0600

    Nearest Vehicle working

--- vue-ui/src/views/Dashboard.vue
+++ vue-ui/src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
-    <v-container class="my-5">
+    <v-container fluid grid-list-md>
       <v-layout row justify-start class="mb-3">
         <v-btn small flat color="grey" @click="resetDemo()">
           <v-icon>cached</v-icon>
@@ -11,7 +11,7 @@
       <v-card flat>
         <v-form class="class px-3">
           <v-layout row wrap>
-            <v-flex xs12 md2>
+            <v-flex xs4>
               <v-radio-group label="Account" v-model="accountCity">
                 <v-radio
                   v-for="account in accounts"
@@ -21,7 +21,7 @@
                 ></v-radio>
               </v-radio-group>
             </v-flex>
-            <v-flex xs12 md2>
+            <v-flex xs4>
               <v-radio-group label="Incident" v-model="selectedIncident">
                 <v-radio
                   v-for="policy in policies"
@@ -37,33 +37,41 @@
       </v-card>
 
       <v-layout row wrap>
-        <v-flex d-flex xs4>
+        <v-flex d-flex xs6>
           <v-card flat>
-            <v-card-title>Trucks</v-card-title>
-            <div class="caption grey--text ml-4">Driver : Location</div>
-            <v-flex d-flex v-for="truck in localTrucks" :key="truck.TruckId" :class="{ 'nearestVehicle': truck.TruckId === nearestVehicleId }">
+            <v-card-title>Vehicles</v-card-title>
+            <v-layout>
+              <v-flex xs1></v-flex>
+              <v-flex xs3 grey--text>Driver</v-flex>
+              <v-flex xs2 grey--text>Location</v-flex>
+            </v-layout>
+            <v-flex
+              d-flex
+              v-for="truck in localTrucks"
+              :key="truck.truckId"
+              :class="nearestVehicleClass(truck.truckId)"
+            >
               <v-layout row wrap>
-                <v-flex>
+                <v-flex d-flex xs1>
                   <v-icon small left>local_shipping</v-icon>
-                  {{ truck.driver }}
                 </v-flex>
-                <v-flex d-flex>{{ truck.lat }} : {{ truck.lng }}</v-flex>
+                <v-flex d-flex xs3>{{ truck.driver }}</v-flex>
+                <v-flex d-flex xs2>{{ truck.lat }} : {{ truck.lng }}</v-flex>
               </v-layout>
             </v-flex>
           </v-card>
         </v-flex>
-        <v-flex xs4>
+        <v-flex xs6>
           <v-card flat>
             <v-card-title>Tickets</v-card-title>
             <v-flex>
               <v-layout row wrap>
-                <v-flex>Ticket 34256 assigned to Michael Jones for Busted HVAC at Gadgets R Us</v-flex>
+                <v-flex>Ticket 34256 assigned to {{nearestVehicleClass(9)}} for {{selectedIncident}} at Gadgets R Us</v-flex>
               </v-layout>
             </v-flex>
           </v-card>
         </v-flex>
       </v-layout>
-      
     </v-container>
 
     <v-container fluid grid-list-md>
@@ -72,11 +80,16 @@
           <v-card flat>
             <v-card-title>Trucks</v-card-title>
             <div class="caption grey--text ml-4">Driver : Location</div>
-            <v-flex d-flex v-for="truck in localTrucks" :key="truck.TruckId" :class="{ 'nearestVehicle': truck.TruckId === nearestVehicleId }">
+            <v-flex
+              d-flex
+              v-for="truck in localTrucks"
+              :key="truck.TruckId"
+              :class="{ 'nearestVehicle': truck.TruckId == nearestVehicleId }"
+            >
               <v-layout row wrap>
                 <v-flex>
                   <v-icon small left>local_shipping</v-icon>
-                  {{ truck.driver }}
+                   {{ nearestVehicleId }} {{ truck.driver }}
                 </v-flex>
                 <v-flex d-flex>{{ truck.lat }} : {{ truck.lng }}</v-flex>
               </v-layout>
@@ -126,7 +139,7 @@ export default {
       // console.log(this.accountCity, this.selectedIncident);
       getLocalTrucks(this.accountCity).then(response => {
         this.localTrucks = response;
-        // TO-DO call find closest vehicle
+        // Find nearest vehicle
         getNearestVehicle(this.localTrucks).then(response => {
           console.log(response);
           this.nearestVehicleId = response;
@@ -138,18 +151,20 @@ export default {
       getAccounts().then(response => {
         this.accounts = response;
       });
+    },
+    nearestVehicleClass(vehicleId) {
+      return vehicleId == this.nearestVehicleId ? 'blue--text' : '';
     }
   },
   mounted() {
     // this.fetchAccounts();
   },
-  computed: {
-  }
+  computed: {}
 };
 </script>
 
 <style>
-  .nearestVehicle {
-    background-color: blue;
-  }
+.nearestVehicle {
+  background-color: blue;
+}
 </style>
